97839063dd7272f2c5dd21bc3a2af233f48b5b74
Bernard065 <bebenibernard@gmail.com>
2023-03-02 22:28:37 +0300

    OverViewCard and OverViewList Components

--- src/pages/Dashbord/OverViewList.js
+++ src/pages/Dashbord/OverViewList.js
@@ -1,8 +1,56 @@
 import React from 'react'
+import './Dashboard.css'
 
 const OverViewList = () => {
   return (
-    <div>OverViewList</div>
+    <div className="container">
+      <div
+        style={{ backgroundColor: "#026995", borderRadius: "5px" }}
+        className="row gx-0 mx-3 px-4 py-1"
+      >
+        <div className="table-headers col">Property Name</div>
+        <div className="table-headers col">Property Type</div>
+        <div className="table-headers col">Rent</div>
+        <div className="table-headers col">Tenant</div>
+        {/* <div className="table-headers col"></div> */}
+      </div>
+      <div
+        style={{ backgroundColor: "#ffffff" }}
+        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
+      >
+        <div className="property_attr col">Silverton Heights</div>
+        <div className="property_attr col">Apartment</div>
+        <div className="property_attr col">Ksh. 10,000</div>
+        <div className="property_attr col">Charlie Dibalde</div>
+      </div>
+      <div
+        style={{ backgroundColor: "#ffffff" }}
+        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
+      >
+        <div className="property_attr col">Juja </div>
+        <div className="property_attr col">Apartment</div>
+        <div className="property_attr col">Ksh. 6,000</div>
+        <div className="property_attr col">Nick ward</div>
+      </div>
+      <div
+        style={{ backgroundColor: "#ffffff" }}
+        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
+      >
+        <div className="property_attr col">Kilimani </div>
+        <div className="property_attr col">Apartments</div>
+        <div className="property_attr col">Ksh. 100,000</div>
+        <div className="property_attr col">Victoria Nase</div>
+      </div>
+      <div
+        style={{ backgroundColor: "#ffffff" }}
+        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
+      >
+        <div className="property_attr col">OuterMall</div>
+        <div className="property_attr col">Mall</div>
+        <div className="property_attr col">Ksh. 10,000,000</div>
+        <div className="property_attr col">Ben K</div>
+      </div>
+    </div>
   )
 }
 
