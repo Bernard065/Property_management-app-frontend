279105e3cad7170550a9768ef652b1674b69b8d9
Bernard065 <bebenibernard@gmail.com>
2023-03-02 22:14:16 +0300

    dashboard component

--- src/pages/Dashbord/Dashboard.js
+++ src/pages/Dashbord/Dashboard.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../Navbar/Navbar'
+import OverViewCard from './OverViewCard'
+import TopNavBar from '../../components/TopNavBar'
+import './Dashboard.css'
 
 const Dashboard = () => {
+
+  const [search, setSearch] = useState("");
+
   return (
-    <div>Dashboard</div>
+    <div className="container-fluid">
+      <div className="row mw-70">
+        <div className="DashboardSideMenu dashboard-nav mh-100 col-2">
+          <Navbar />
+        </div>
+        <div
+          style={{
+            backgroundColor: "#eaf9ff",
+            maxHeight: "100%",
+            overflowY: "scroll",
+          }}
+          className="dashboard-main mh-100 col-10"
+        >
+          <TopNavBar setSearch={setSearch} search={search} />
+          <div className="Greeting-text row mx-5 my-4">
+            <p>Hello, Chris</p>
+          </div>
+          <OverViewCard />
+        </div>
+      </div>
+    </div>
   )
 }
 
