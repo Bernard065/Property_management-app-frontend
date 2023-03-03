d80cfec7da1e43b0f39d19c6b083c64215f1b228
Bernard065 <bebenibernard@gmail.com>
2023-03-03 16:04:51 +0300

    displaying five entries of properties in the dashboard

--- src/pages/Dashbord/OverViewList.js
+++ src/pages/Dashbord/OverViewList.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Dashboard.css'
 
 const OverViewList = () => {
+
+  const [property, setProperty] = useState([]);
+  const baseUrl = "http://localhost:9292/";
+  useEffect(() => {
+    getProperties();
+  }, []);
+  function getProperties() {
+    fetch(`${baseUrl}properties?limit=5`)
+      .then((res) => res.json())
+      .then((data) => {
+        setProperty(data);
+      });
+  }
+
+  const tableData = property.slice(0, 5).map((property) => {
+    return (
+      <div
+        key={property.id}
+        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
+      >
+        <div className="property_attr col">{property.property_name}</div>
+        <div className="property_attr col">{property.location}</div>
+        <div className="property_attr col">{property.property_type}</div>
+        <div className="property_attr col">{property.property_size}</div>
+      </div>
+    );
+  });
+  
+
   return (
     <div className="container">
       <div
@@ -11,45 +40,10 @@ const OverViewList = () => {
         <div className="table-headers col">Property Name</div>
         <div className="table-headers col">Property Type</div>
         <div className="table-headers col">Rent</div>
-        <div className="table-headers col">Tenant</div>
+        <div className="table-headers col">Property-size</div>
         {/* <div className="table-headers col"></div> */}
       </div>
-      <div
-        style={{ backgroundColor: "#ffffff" }}
-        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
-      >
-        <div className="property_attr col">Silverton Heights</div>
-        <div className="property_attr col">Apartment</div>
-        <div className="property_attr col">Ksh. 10,000</div>
-        <div className="property_attr col">Charlie Dibalde</div>
-      </div>
-      <div
-        style={{ backgroundColor: "#ffffff" }}
-        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
-      >
-        <div className="property_attr col">Juja </div>
-        <div className="property_attr col">Apartment</div>
-        <div className="property_attr col">Ksh. 6,000</div>
-        <div className="property_attr col">Nick ward</div>
-      </div>
-      <div
-        style={{ backgroundColor: "#ffffff" }}
-        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
-      >
-        <div className="property_attr col">Kilimani </div>
-        <div className="property_attr col">Apartments</div>
-        <div className="property_attr col">Ksh. 100,000</div>
-        <div className="property_attr col">Victoria Nase</div>
-      </div>
-      <div
-        style={{ backgroundColor: "#ffffff" }}
-        className="PropertyRow row gx-0 mx-3 my-3 px-4 py-1"
-      >
-        <div className="property_attr col">OuterMall</div>
-        <div className="property_attr col">Mall</div>
-        <div className="property_attr col">Ksh. 10,000,000</div>
-        <div className="property_attr col">Ben K</div>
-      </div>
+        { tableData }
     </div>
   )
 }
